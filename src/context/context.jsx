488eba2f8b0dad2a1a6510760c902fb5c91a9382
SiddareddyRajeshReddy <b230065@nitsikkkim.ac.in>
2025-07-17 12:36:26 +0530

    test

---
+++ src/context/context.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { createContext } from 'react'
+const routerContext = createContext(0)
+export default routerContext
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/home'
+import CelebrationBackground from './components/celebrationPage'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "celeb",
+    element: <CelebrationBackground/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
